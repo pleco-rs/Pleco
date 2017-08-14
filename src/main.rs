821f5afd3d10a78e29b4e1c8145fd5c7567bbaab
Andrew Brinker <me@andrewbrinker.com>
2017-08-14 16:59:42 -0700

    Fix some clippy lints

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,9 @@ extern crate rand;
 use pleco::templates::print_bitboard;
 use pleco::engine::*;
 
-use pleco::{board,piece_move,templates,timer};
-use pleco::bots::bot_minimax::SimpleBot;
+use pleco::{board, piece_move, templates, timer};
 use pleco::bots::bot_random::RandomBot;
-use pleco::bots::bot_parallel_minimax::ParallelSearcher;
 use pleco::bots::bot_advanced::AdvancedBot;
-use pleco::bots::bot_alphabeta::AlphaBetaBot;
 use pleco::bots::bot_jamboree::JamboreeSearcher;
 use pleco::bots::bot_expert::ExpertBot;
 
@@ -22,16 +19,16 @@ use pleco::bots::bot_expert::ExpertBot;
 // 1r1qkbn1/p2B2pr/b4QP1/1ppp4/P2n1P1p/2P5/1P2P2P/RNB1K1NR b KQ - 2 16
 
 fn main() {
-//    gen_random_fens();
+    //    gen_random_fens();
     sample_run();
-//    compete_multiple(AdvancedBot{}, ExpertBot{}, 60, 10, 5, true);
+    //    compete_multiple(AdvancedBot{}, ExpertBot{}, 60, 10, 5, true);
 
 }
 
 fn test_between() {
-    let mut b = board::Board::default();
+    let b = board::Board::default();
     let m = b.magic_helper;
-    print_bitboard(m.between_bb(24,60));
+    print_bitboard(m.between_bb(24, 60));
 }
 
 fn sample_run() {
@@ -47,14 +44,15 @@ fn sample_run() {
             i = max;
         } else {
             if i % 57 == 2 {
-                let mov = RandomBot::best_move(b.shallow_clone(),&timer::Timer::new(20));
-                println!("{}'s move: {}",RandomBot::name(),mov);
+                let mov = RandomBot::best_move(b.shallow_clone(), &timer::Timer::new(20));
+                println!("{}'s move: {}", RandomBot::name(), mov);
                 b.apply_move(mov);
             } else if i % 2 == 0 {
                 println!("------------------------------------------------");
                 println!();
-                let mov = AdvancedBot::best_move_depth(b.shallow_clone(),&timer::Timer::new(20),5);
-                println!("{}'s move: {}",AdvancedBot::name(),mov);
+                let mov =
+                    AdvancedBot::best_move_depth(b.shallow_clone(), &timer::Timer::new(20), 5);
+                println!("{}'s move: {}", AdvancedBot::name(), mov);
                 b.apply_move(mov);
             } else {
                 let mov = ExpertBot::best_move_depth(b.shallow_clone(), &timer::Timer::new(20), 5);
@@ -75,23 +73,22 @@ fn sample_run() {
 fn gen_random_fens() {
     let mut b = board::Board::default();
     println!("[");
-    println!("\"{}\",",b.get_fen());
+    println!("\"{}\",", b.get_fen());
 
     let quota = 4;
-    let moves = 0;
 
     let max = 200;
     let mut i = 0;
 
-    let mut beginning_count = 0;
+    let beginning_count = 0;
     let mut middle_count = 0;
     let mut end_count = 0;
 
     while beginning_count + middle_count + end_count <= (quota * 3) - 1 {
         if i == 0 {
-            let mov = RandomBot::best_move_depth(b.shallow_clone(),&timer::Timer::new(20),1);
+            let mov = RandomBot::best_move_depth(b.shallow_clone(), &timer::Timer::new(20), 1);
             b.apply_move(mov);
-            let mov = RandomBot::best_move_depth(b.shallow_clone(),&timer::Timer::new(20),1);
+            let mov = RandomBot::best_move_depth(b.shallow_clone(), &timer::Timer::new(20), 1);
             b.apply_move(mov);
         }
         if b.checkmate() || i > max {
@@ -103,13 +100,15 @@ fn gen_random_fens() {
             }
         } else {
             if i % 11 == 9 {
-                let mov = RandomBot::best_move_depth(b.shallow_clone(),&timer::Timer::new(20),1);
+                let mov = RandomBot::best_move_depth(b.shallow_clone(), &timer::Timer::new(20), 1);
                 b.apply_move(mov);
             } else if i % 2 == 0 {
-                let mov = JamboreeSearcher::best_move_depth(b.shallow_clone(),&timer::Timer::new(20),5);
+                let mov =
+                    JamboreeSearcher::best_move_depth(b.shallow_clone(), &timer::Timer::new(20), 5);
                 b.apply_move(mov);
             } else {
-                let mov = AdvancedBot::best_move_depth(b.shallow_clone(), &timer::Timer::new(20), 5);
+                let mov =
+                    AdvancedBot::best_move_depth(b.shallow_clone(), &timer::Timer::new(20), 5);
                 b.apply_move(mov);
             }
             i += 1;
@@ -117,13 +116,13 @@ fn gen_random_fens() {
 
         if b.zobrist() % 23 == 11 && b.moves_played() > 7 {
             if b.count_all_pieces() < 13 && end_count < quota {
-                println!("\"{}\",",b.get_fen());
+                println!("\"{}\",", b.get_fen());
                 end_count += 1;
             } else if b.count_all_pieces() < 24 && middle_count < quota {
-                println!("\"{}\",",b.get_fen());
+                println!("\"{}\",", b.get_fen());
                 middle_count += 1;
             } else if beginning_count < quota {
-                println!("\"{}\",",b.get_fen());
+                println!("\"{}\",", b.get_fen());
                 middle_count += 1;
             }
         }
@@ -137,7 +136,7 @@ fn test_moving() {
     let p = piece_move::PreMoveInfo {
         src: 12,
         dst: 28,
-        flags: piece_move::MoveFlag::DoublePawnPush
+        flags: piece_move::MoveFlag::DoublePawnPush,
     };
     let m = piece_move::BitMove::init(p);
     b.fancy_print();
@@ -146,7 +145,7 @@ fn test_moving() {
     let p = piece_move::PreMoveInfo {
         src: 51,
         dst: 35,
-        flags: piece_move::MoveFlag::DoublePawnPush
+        flags: piece_move::MoveFlag::DoublePawnPush,
     };
     let m = piece_move::BitMove::init(p);
     b.apply_move(m);
@@ -154,7 +153,7 @@ fn test_moving() {
     let p = piece_move::PreMoveInfo {
         src: 28,
         dst: 35,
-        flags: piece_move::MoveFlag::Capture {ep_capture: false}
+        flags: piece_move::MoveFlag::Capture { ep_capture: false },
     };
     let m = piece_move::BitMove::init(p);
     b.apply_move(m);
@@ -167,7 +166,7 @@ fn test_moving() {
     let p = piece_move::PreMoveInfo {
         src: 59,
         dst: 35,
-        flags: piece_move::MoveFlag::Capture {ep_capture: false}
+        flags: piece_move::MoveFlag::Capture { ep_capture: false },
     };
     let m = piece_move::BitMove::init(p);
     b.apply_move(m);
@@ -185,7 +184,7 @@ fn test_moving() {
     let p = piece_move::PreMoveInfo {
         src: 35,
         dst: 8,
-        flags: piece_move::MoveFlag::Capture {ep_capture: false}
+        flags: piece_move::MoveFlag::Capture { ep_capture: false },
     };
     let m = piece_move::BitMove::init(p);
     b.apply_move(m);
@@ -194,7 +193,7 @@ fn test_moving() {
     let p = piece_move::PreMoveInfo {
         src: 6,
         dst: 21,
-        flags: piece_move::MoveFlag::QuietMove
+        flags: piece_move::MoveFlag::QuietMove,
     };
     let m = piece_move::BitMove::init(p);
     b.apply_move(m);
@@ -203,7 +202,7 @@ fn test_moving() {
     let p = piece_move::PreMoveInfo {
         src: 60,
         dst: 59,
-        flags: piece_move::MoveFlag::QuietMove
+        flags: piece_move::MoveFlag::QuietMove,
     };
     let m = piece_move::BitMove::init(p);
     b.apply_move(m);
@@ -212,7 +211,7 @@ fn test_moving() {
     let p = piece_move::PreMoveInfo {
         src: 4,
         dst: 7,
-        flags: piece_move::MoveFlag::Castle{king_side: true}
+        flags: piece_move::MoveFlag::Castle { king_side: true },
     };
     let m = piece_move::BitMove::init(p);
     b.apply_move(m);
@@ -221,7 +220,7 @@ fn test_moving() {
     let p = piece_move::PreMoveInfo {
         src: templates::Square::A2 as u8,
         dst: templates::Square::B1 as u8,
-        flags: piece_move::MoveFlag::Capture {ep_capture: false}
+        flags: piece_move::MoveFlag::Capture { ep_capture: false },
     };
     let m = piece_move::BitMove::init(p);
     b.apply_move(m);
@@ -230,7 +229,7 @@ fn test_moving() {
     let p = piece_move::PreMoveInfo {
         src: templates::Square::A2 as u8,
         dst: templates::Square::B1 as u8,
-        flags: piece_move::MoveFlag::Capture {ep_capture: false}
+        flags: piece_move::MoveFlag::Capture { ep_capture: false },
     };
     let m = piece_move::BitMove::init(p);
     b.apply_move(m);
@@ -242,10 +241,10 @@ fn test_moving() {
 
     let moves = b.generate_moves();
 
-    for x in moves.iter() {
-        println!("{}",x)
+    for x in &moves {
+        println!("{}", x)
     }
 
-    println!("{}",moves.len());
+    println!("{}", moves.len());
 
 }
--- tests/fen_building.rs
+++ tests/fen_building.rs
@@ -12,39 +12,38 @@ use std::*;
 fn multiple_fens() {
     // Test if positions int he right place
     let board = Board::new_from_fen("k6r/1p2b3/8/8/8/8/P4KPP/1B5R w KQkq - 0 3");
-    assert_eq!(board.count_piece(Player::White, Piece::P),  3);
-    assert_eq!(board.count_piece(Player::White, Piece::N),  0);
-    assert_eq!(board.count_piece(Player::White, Piece::B),  1);
-    assert_eq!(board.count_piece(Player::White, Piece::R),  1);
-    assert_eq!(board.count_piece(Player::White, Piece::Q),  0);
-    assert_eq!(board.count_piece(Player::White, Piece::K),  1);
-    assert_eq!(board.count_piece(Player::Black, Piece::P),  1);
-    assert_eq!(board.count_piece(Player::Black, Piece::N),  0);
-    assert_eq!(board.count_piece(Player::Black, Piece::B),  1);
-    assert_eq!(board.count_piece(Player::Black, Piece::R),  1);
-    assert_eq!(board.count_piece(Player::Black, Piece::Q),  0);
-    assert_eq!(board.count_piece(Player::Black, Piece::K),  1);
+    assert_eq!(board.count_piece(Player::White, Piece::P), 3);
+    assert_eq!(board.count_piece(Player::White, Piece::N), 0);
+    assert_eq!(board.count_piece(Player::White, Piece::B), 1);
+    assert_eq!(board.count_piece(Player::White, Piece::R), 1);
+    assert_eq!(board.count_piece(Player::White, Piece::Q), 0);
+    assert_eq!(board.count_piece(Player::White, Piece::K), 1);
+    assert_eq!(board.count_piece(Player::Black, Piece::P), 1);
+    assert_eq!(board.count_piece(Player::Black, Piece::N), 0);
+    assert_eq!(board.count_piece(Player::Black, Piece::B), 1);
+    assert_eq!(board.count_piece(Player::Black, Piece::R), 1);
+    assert_eq!(board.count_piece(Player::Black, Piece::Q), 0);
+    assert_eq!(board.count_piece(Player::Black, Piece::K), 1);
 
 
     let board = Board::new_from_fen("8/2Q1pk2/nbpppppp/8/8/2K4N/PPPPPPPP/BBB2BBB w ---- a3 0 10");
-    assert_eq!(board.count_piece(Player::White, Piece::P),  8);
-    assert_eq!(board.count_piece(Player::White, Piece::N),  1);
-    assert_eq!(board.count_piece(Player::White, Piece::B),  6);
-    assert_eq!(board.count_piece(Player::White, Piece::R),  0);
-    assert_eq!(board.count_piece(Player::White, Piece::Q),  1);
-    assert_eq!(board.count_piece(Player::White, Piece::K),  1);
-    assert_eq!(board.count_piece(Player::Black, Piece::P),  7);
-    assert_eq!(board.count_piece(Player::Black, Piece::N),  1);
-    assert_eq!(board.count_piece(Player::Black, Piece::B),  1);
-    assert_eq!(board.count_piece(Player::Black, Piece::R),  0);
-    assert_eq!(board.count_piece(Player::Black, Piece::Q),  0);
-    assert_eq!(board.count_piece(Player::Black, Piece::K),  1);
+    assert_eq!(board.count_piece(Player::White, Piece::P), 8);
+    assert_eq!(board.count_piece(Player::White, Piece::N), 1);
+    assert_eq!(board.count_piece(Player::White, Piece::B), 6);
+    assert_eq!(board.count_piece(Player::White, Piece::R), 0);
+    assert_eq!(board.count_piece(Player::White, Piece::Q), 1);
+    assert_eq!(board.count_piece(Player::White, Piece::K), 1);
+    assert_eq!(board.count_piece(Player::Black, Piece::P), 7);
+    assert_eq!(board.count_piece(Player::Black, Piece::N), 1);
+    assert_eq!(board.count_piece(Player::Black, Piece::B), 1);
+    assert_eq!(board.count_piece(Player::Black, Piece::R), 0);
+    assert_eq!(board.count_piece(Player::Black, Piece::Q), 0);
+    assert_eq!(board.count_piece(Player::Black, Piece::K), 1);
 }
 
 #[test]
 pub fn test_fens() {
-    for str in TEST_FENS.iter() {
+    for str in &TEST_FENS {
         Board::new_from_fen(str);
     }
 }
-
