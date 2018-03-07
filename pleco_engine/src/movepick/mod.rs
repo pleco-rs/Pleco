8f3f2cad179f5efffbfc772f80568c00a46039c1
stephenf <stephenf@cs.washington.edu>
2018-03-07 02:12:43 -0800

    Basic MovePicker down. Methods for Main Search, Evasions, QSearch, and ProbCut in place. Need to test it though.
    Signed-off-by: stephenf <stephenf@cs.washington.edu>

--- pleco_engine/src/movepick/mod.rs
+++ pleco_engine/src/movepick/mod.rs
@@ -1,8 +1,10 @@
 mod pick;
 
 use std::ptr;
+use std::mem;
+
 #[allow(unused_imports)]
-use pleco::{BitMove,Board,ScoringMove,ScoringMoveList};
+use pleco::{BitMove,Board,ScoringMove,ScoringMoveList,SQ};
 use pleco::board::movegen::{PseudoLegal,MoveGen};
 use pleco::helper::prelude::piece_value;
 use pleco::core::mono_traits::*;
@@ -15,36 +17,114 @@ pub struct MovePicker {
     pick: Pick,
     board: *const Board,
     moves: ScoringMoveList,
-    depth: u16,
+    depth: i16,
     ttm: BitMove,
     killers: [BitMove; 2],
     cm: BitMove,
-
+    recapture_sq: SQ,
+    threshold: i32,
     cur_ptr: *mut ScoringMove,
     end_ptr: *mut ScoringMove,
     end_bad_captures: *mut ScoringMove,
 }
 
 impl MovePicker {
-    pub fn main_search(board: &Board, depth: u16, mut ttm: BitMove,
+    /// MovePicker constructor for the main search
+    pub fn main_search(board: &Board, depth: i16, mut ttm: BitMove,
                        killers: &[BitMove; 2], counter_move: BitMove) -> Self {
-        assert!(!board.in_check());
+
+        let mut pick = if board.in_check() {Pick::EvasionSearch} else {Pick::MainSearch};
 
         if ttm == BitMove::null() || !board.pseudo_legal_move(ttm) {
             ttm = BitMove::null();
+            pick.incr();
         }
 
         let mut moves = ScoringMoveList::default();
         let first: *mut ScoringMove = moves.as_mut_ptr();
 
         MovePicker {
-            pick: Pick::MainSearch,
+            pick,
             board: &* board,
             moves,
             depth,
             ttm,
             killers: (*killers).clone(),
             cm: counter_move,
+            recapture_sq: unsafe {mem::uninitialized()},
+            threshold: unsafe {mem::uninitialized()},
+            cur_ptr: first,
+            end_ptr: first,
+            end_bad_captures: first,
+        }
+    }
+
+    /// MovePicker constructor for quiescence search
+    pub fn qsearch(board: &Board, depth: i16, ttm: BitMove, recapture_sq: SQ) -> Self {
+        assert!(depth <= 0);
+        let mut moves = ScoringMoveList::default();
+        let first: *mut ScoringMove = moves.as_mut_ptr();
+        let mut mp_qs = MovePicker {
+            pick: unsafe { mem::uninitialized() },
+            board: &*board,
+            moves,
+            depth,
+            ttm,
+            killers: unsafe { mem::uninitialized() },
+            cm: unsafe { mem::uninitialized() },
+            recapture_sq: unsafe { mem::uninitialized() },
+            threshold: unsafe {mem::uninitialized()},
+            cur_ptr: first,
+            end_ptr: first,
+            end_bad_captures: first,
+        };
+
+        if board.in_check() {
+            mp_qs.pick = Pick::EvasionSearch;
+        } else if depth > -5 {
+            mp_qs.pick = Pick::QSearch;
+        } else {
+            mp_qs.pick = Pick::QSearchRecaptures;
+            mp_qs.recapture_sq = recapture_sq;
+            return mp_qs;
+        }
+
+        if ttm == BitMove::null() || !board.pseudo_legal_move(ttm) {
+            mp_qs.ttm = BitMove::null();
+            mp_qs.pick.incr()
+        }
+
+        mp_qs
+    }
+
+    /// MovePicker constructor for ProbCut: we generate captures with SEE higher
+    /// than or equal to the given threshold.
+    pub fn probcut_search(board: &Board, threshold: i32, mut ttm: BitMove, recapture_sq: SQ) -> Self {
+        assert!(board.in_check());
+        let mut moves = ScoringMoveList::default();
+        let first: *mut ScoringMove = moves.as_mut_ptr();
+
+        let mut pick = Pick::ProbCutSearch;
+
+        ttm = if ttm != BitMove::null()
+            && board.pseudo_legal_move(ttm)
+            && ttm.is_capture() {
+            ttm
+        } else {
+            pick.incr();
+            BitMove::null()
+        };
+
+        MovePicker {
+            pick,
+            board: &*board,
+            moves,
+            depth: unsafe { mem::uninitialized() },
+            ttm,
+            killers: unsafe { mem::uninitialized() },
+            cm: unsafe { mem::uninitialized() },
+            recapture_sq: recapture_sq,
+            threshold,
             cur_ptr: first,
             end_ptr: first,
             end_bad_captures: first,
@@ -60,6 +140,22 @@ impl MovePicker {
                 let piece_cap = board.captured_piece(mov).unwrap();
                 (*ptr).score = piece_value(piece_cap,false) as i16
                     - piece_value(piece_moved,false) as i16;
+                ptr = ptr.add(1);
+            }
+        }
+    }
+
+    fn score_evasions(&mut self, board: &Board) {
+        let mut ptr = self.cur_ptr;
+        unsafe {
+            while ptr < self.end_ptr {
+                let mov: BitMove = (*ptr).bit_move;
+                if mov.is_capture() {
+                    let piece_moved = board.moved_piece(mov);
+                    let piece_cap = board.captured_piece(mov).unwrap();
+                    (*ptr).score = piece_value(piece_cap,false) as i16
+                        - piece_value(piece_moved,false) as i16;
+                }
                 ptr = ptr.add(1);
             }
         }
@@ -84,18 +180,20 @@ impl MovePicker {
     pub fn next(&mut self, board: &Board, skip_quiets: bool) -> BitMove {
         let mut mov: ScoringMove = ScoringMove::null();
         match self.pick {
-            Pick::MainSearch => {
+            Pick::MainSearch | Pick::EvasionSearch | Pick::QSearch | Pick::ProbCutSearch => {
                 self.pick.incr();
                 return self.ttm;
             },
-            Pick::CapturesInit => {
+            Pick::CapturesInit | Pick::ProbCutCapturesInit | Pick::QSearchInit | Pick::QSearchRecaptures => {
                 unsafe {
+                    self.end_bad_captures = self.moves.as_mut_ptr();
+                    self.cur_ptr = self.end_bad_captures;
                     self.end_ptr = MoveGen::extend_from_ptr::<PseudoLegal,CapturesGenType, ScoringMoveList>
                         (board, self.cur_ptr);
                 }
                 self.score_captures(board);
                 self.pick.incr();
-                self.next(board, skip_quiets)
+                return self.next(board, skip_quiets);
             },
             Pick::GoodCaptures => {
                 while self.cur_ptr < self.end_ptr {
@@ -110,7 +208,7 @@ impl MovePicker {
                     }
                 }
                 self.pick.incr();
-                self.next(board, skip_quiets)
+                return self.next(board, skip_quiets);
             },
             Pick::KillerOne | Pick::KillerTwo => {
                 mov.bit_move = self.killers[self.pick as usize - Pick::KillerOne as usize];
@@ -121,7 +219,7 @@ impl MovePicker {
                     && !mov.bit_move.is_capture() {
                     return mov.bit_move;
                 }
-                self.next(board, skip_quiets)
+                return self.next(board, skip_quiets);
             },
             Pick::CounterMove => {
                 self.pick.incr();
@@ -133,7 +231,7 @@ impl MovePicker {
                     && !self.cm.is_capture() {
                     return self.cm;
                 }
-                self.next(board, skip_quiets)
+                return self.next(board, skip_quiets);
             },
             Pick::QuietInit => {
                 unsafe {
@@ -142,10 +240,109 @@ impl MovePicker {
                         board, self.cur_ptr);
                     // TODO: Need to score the captures
                 }
-                return BitMove::null();
+                self.pick.incr();
+                return self.next(board, skip_quiets);
+            },
+            Pick::QuietMoves => {
+                if !skip_quiets {
+                    while self.cur_ptr < self.end_ptr {
+                        unsafe {
+                            mov = *self.cur_ptr;
+                            self.cur_ptr = self.cur_ptr.add(1);
+                            if mov.bit_move != self.ttm
+                                && mov.bit_move != self.killers[0]
+                                && mov.bit_move != self.killers[1]
+                                && mov.bit_move != self.cm {
+                                return mov.bit_move;
+                            }
+                        }
+                    }
+                }
+                self.pick.incr();
+                self.cur_ptr = self.moves.as_mut_ptr();
+                return self.next(board, skip_quiets);
+            },
+            Pick::BadCaptures => {
+                if self.cur_ptr < self.end_bad_captures {
+                    unsafe  {
+                        mov = *self.cur_ptr;
+                        self.cur_ptr = self.cur_ptr.add(1);
+                    }
+                    return mov.bit_move
+                }
+            },
+            Pick::EvasionsInit => {
+                unsafe {
+                    self.cur_ptr = self.moves.as_mut_ptr();
+                    self.end_ptr = MoveGen::extend_from_ptr::<PseudoLegal,EvasionsGenType,ScoringMoveList>
+                        (board, self.cur_ptr);
+                }
+                self.score_evasions(board);
+                self.pick.incr();
+                return self.next(board, skip_quiets);
+            },
+            Pick::AllEvasions => {
+                while self.cur_ptr < self.end_ptr {
+                    mov = self.pick_best(self.cur_ptr, self.end_ptr);
+                    unsafe {self.cur_ptr = self.cur_ptr.add(1);}
+                    if mov.bit_move != self.ttm {
+                        return mov.bit_move;
+                    }
+                }
+            },
+            Pick::ProbCutCaptures => {
+                while self.cur_ptr < self.end_ptr {
+                    mov = self.pick_best(self.cur_ptr, self.end_ptr);
+                    unsafe {self.cur_ptr = self.cur_ptr.add(1);}
+                    if mov.bit_move != self.ttm {
+                        return mov.bit_move;
+                    }
+                }
+            },
+            Pick::QCaptures => {
+                while self.cur_ptr < self.end_ptr {
+                    mov = self.pick_best(self.cur_ptr, self.end_ptr);
+                    unsafe {self.cur_ptr = self.cur_ptr.add(1);}
+                    if mov.bit_move != self.ttm {
+                        return mov.bit_move;
+                    }
+                }
+
+                if self.depth > -1 {
+                    unsafe {
+                        self.cur_ptr = self.moves.as_mut_ptr();
+                        self.end_ptr = MoveGen::extend_from_ptr::<PseudoLegal,QuietChecksGenType,ScoringMoveList>
+                            (board, self.cur_ptr);
+                    }
+                    self.pick.incr();
+                    return self.next(board, skip_quiets);
+                }
+            },
+            Pick::QChecks => {
+                while self.cur_ptr < self.end_ptr {
+                    unsafe {
+                        mov = *self.cur_ptr;
+                        self.cur_ptr = self.cur_ptr.add(1);
+                    }
+                    if mov.bit_move != self.ttm {
+                        return mov.bit_move;
+                    }
+                }
+
+            },
+            Pick::QRecaptures => {
+                while self.cur_ptr < self.end_ptr {
+                    unsafe {
+                        mov = *self.cur_ptr;
+                        self.cur_ptr = self.cur_ptr.add(1);
+                        if mov.bit_move.get_dest() == self.recapture_sq {
+                            return mov.bit_move;
+                        }
+                    }
+                }
             }
-            _ => BitMove::null()
         }
+        BitMove::null()
     }
 }
 
